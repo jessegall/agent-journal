e44f1d6e68ceedf0ecb2924a611aac8c9efbd642
Jesse Gall <dev@jessegall.nl>
2026-09-21 05:51:58 +0200

    The events listing returns one page unless asked for more

--- commands/http.py
+++ commands/http.py
@@ -323,7 +323,7 @@ def get_extension_zip(req: Request) -> Reply:
 
 @route("GET", "/api/{env}/events")
 def get_events(req: Request) -> Reply:
-    return Reply(200, [asdict(e) for e in req.record().events(int(req.query.get("since") or 0), int(req.query.get("last") or 0))])
+    return Reply(200, [asdict(e) for e in req.record().events(int(req.query.get("since") or 0), int(req.query.get("last") or LAST))])
 
 
 @route("GET", "/api/{env}/settings")
